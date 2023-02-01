00bf6fffd108ab690e7d2314422e143375b6e163
lsagolj <sagoljluka@gmail.com>
2023-02-01 20:46:30 +0100

    ktm

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include "mbed.h"
+#include "main.h"
 
-// main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    fist_pwm.period_ms(20);
+    shoulder_pwm.period_ms(20);
+    elbow_pwm.period_ms(20);
 
+    while(true)
+    {
+        for(int i = 500; i < 2300; i += 1)
+        {
+            elbow_pwm.pulsewidth_us(0 + i);
+            ThisThread::sleep_for(2);
+        }
+
+        for(int i = 0; i < 1800; i += 1)
+        {
+           elbow_pwm.pulsewidth_us(2300 - i);
+           ThisThread::sleep_for(2);
+        }
     }
 }
-
---
+++ main.h
@@ -0,0 +1,5 @@
+#include "mbed.h"
+
+PwmOut fist_pwm(D4);
+PwmOut shoulder_pwm(D3);
+PwmOut elbow_pwm(D2);
